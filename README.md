818c2de5a969f15d6484509e543ce21af176d29a
Akerele, Tosin James <akerele.james1699@gmail.com>
2021-01-09 08:35:42 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-https://akerele180.github.io/article-preview-component-masterr/
-
 # Frontend Mentor - Article preview component
 
 ![Design preview for the Article preview component coding challenge](./design/desktop-preview.jpg)
--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 const socials = document.querySelector('.social-links');
-const btn = document.getElementById('btn-share');
-// const btn = document.querySelector('share-button');
-const footer = document.querySelector('.footer');
+const button = document.querySelector('.share-btn');
+const socials2 = document.querySelector("svg");
+const container = document.querySelector('.container-mini');
 
-socials.style.visibility = "hidden";
-btn.addEventListener('click', function(){
-    socials.style.visibility = "visible";
-});
+socials.style.display = 'none';
+button.addEventListener('click', ()=>{
+    // socials.style.visibility = 'visible';
+    socials.style.display='flex';
+    container.style.transform="translateY(29px)"
+})
+socials2.addEventListener('click', ()=>{
+    socials.style.display="none";
+    container.style.transform="translateY(0px)"
+})
+
+
+// function myFunction() {
+//     var x = document.getElementById("myDIV");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
