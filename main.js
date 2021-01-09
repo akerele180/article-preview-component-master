261da814ffb28143a83bd64c797da7640f444384
Akerele, Tosin James <akerele.james1699@gmail.com>
2021-01-09 12:48:21 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,17 +2,41 @@ const socials = document.querySelector('.social-links');
 const button = document.querySelector('.share-btn');
 const socials2 = document.querySelector("svg");
 const container = document.querySelector('.container-mini');
+var viewPort = window.matchMedia("(min-width: 701px)");
 
 socials.style.display = 'none';
 button.addEventListener('click', ()=>{
     // socials.style.visibility = 'visible';
-    socials.style.display='flex';
-    container.style.transform="translateY(29px)"
+    if (socials.style.display==='flex'){
+        socials.style.display="none";
+    }else {
+
+        socials.style.display='flex';
+    }
+    if (viewPort.matches) { // If media query matches
+        container.style.transform="translateY(0)";
+      } else {
+        container.style.transform="translateY(29px)";
+      }
 })
 socials2.addEventListener('click', ()=>{
     socials.style.display="none";
     container.style.transform="translateY(0px)"
 })
+
+
+// function myFunction(x) {
+//     if (x.matches) { // If media query matches
+//       document.body.style.backgroundColor = "yellow";
+//     } else {
+//       document.body.style.backgroundColor = "pink";
+//     }
+//   }
+  
+//   var x = window.matchMedia("(max-width: 700px)")
+//   myFunction(x) // Call listener function at run time
+//   x.addListener(myFunction) // Attach listener function on state changes
+
 
 
 // function myFunction() {
